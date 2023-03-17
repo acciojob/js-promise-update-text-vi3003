d3ee89e6021730183151a16b0c0ada3e6fed7497
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-17 15:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+
+Hello().then((res) => {
+   output.textContent = res;
+});
